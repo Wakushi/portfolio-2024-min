9f002c53eab8617ce93e074083b9f4ebf538f21e
makushi <maxime.eliazord@sequodia.com>
2024-02-24 05:42:23 +0100

    Animated blocks

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,21 @@ import { GoArrowUpRight } from "react-icons/go"
 import { MdAlternateEmail } from "react-icons/md"
 
 export default function Home() {
+  const animationDelayIncrement = 0.1
+  let animationDelay = 0
+
+  const generateDelayStyle = () => {
+    const style = { animation: `fade-in 1s ease ${animationDelay}s forwards` }
+    animationDelay += animationDelayIncrement
+    return style
+  }
   return (
     <main className="p-4 pb-8 lg:pt-[8rem] mx-auto max-w-[640px]">
       <article className="flex flex-col gap-8 mb-4">
-        <div className="flex flex-col md:flex-row items-center justify-between">
+        <div
+          style={generateDelayStyle()}
+          className="flex flex-col md:flex-row items-center justify-between animated-block"
+        >
           <h1 className="text-[3rem]">Maxime Eliazord</h1>
           <div className="flex items-center gap-4">
             <a href="https://github.com/Wakushi" target="_blank">
@@ -31,19 +42,22 @@ export default function Home() {
             </a>
           </div>
         </div>
-        <p>
+        <p style={generateDelayStyle()} className="animated-block">
           <em className="font-extralight">Solving functional needs.</em>{" "}
           Passionate for crafting accessible, polished and secure experiences.
           Creating tailor-made web applications to help people increase their
           productivity and profitability.
         </p>
-        <p>
+        <p style={generateDelayStyle()} className="animated-block">
           Web developer at <a href="https://sequodia.com/">Sequodia</a>,
           building <a href="https://ethereum.org/">on-chain</a> on my spare
           time.
         </p>
         <div className="flex flex-col md:flex-row justify-between gap-10 mb-4">
-          <div className="flex flex-col gap-6 flex-1">
+          <div
+            style={generateDelayStyle()}
+            className="flex flex-col gap-6 flex-1 animated-block"
+          >
             <h2 className="text-sm text-dark">Building</h2>
             <div className="flex flex-col gap-2">
               <HoverCard>
@@ -83,7 +97,10 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="flex flex-col gap-6 flex-1">
+          <div
+            style={generateDelayStyle()}
+            className="flex flex-col gap-6 flex-1 animated-block"
+          >
             <h2 className="text-sm text-dark">Projects</h2>
             <div className="flex flex-col gap-2">
               <div className="flex items-center">
@@ -120,7 +137,10 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="flex flex-col gap-6 flex-1">
+          <div
+            style={generateDelayStyle()}
+            className="flex flex-col gap-6 flex-1 animated-block"
+          >
             <h2 className="text-sm text-dark">Hacks</h2>
             <div className="flex flex-col gap-2">
               <HoverCard>
@@ -170,8 +190,13 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <h2>Why ?</h2>
-        <div className="flex flex-col gap-2">
+        <h2 style={generateDelayStyle()} className="animated-block">
+          Why ?
+        </h2>
+        <div
+          style={generateDelayStyle()}
+          className="flex flex-col gap-2 animated-block"
+        >
           <p>
             Building on the web is <em className="font-extralight">art</em>.
           </p>
